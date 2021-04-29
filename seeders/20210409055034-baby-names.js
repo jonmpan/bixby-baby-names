2a26195ef57c4efd2ed7c16098ad15bd01c7daae
jonmpan <jonmpan@gmail.com>
2021-04-28 17:20:55 -0700

    updated seed file

--- seeders/20210409055034-baby-names.js
+++ seeders/20210409055034-baby-names.js
@@ -1,30 +1,45 @@
 "use strict";
 const models = require("../models");
-const { parseDataFiles } = require("../src/utils");
+const { Names } = models;
+const fs = require("fs-promise");
+
+const parseDataFiles = async (file) => {
+  console.time("getting all names");
+  const allNames = [];
+  let namesChecked = 0;
+  console.log("hello from parser");
+  var files = await fs.readdir(__dirname + "/../names");
+  for (const fileName of files) {
+    const year = fileName.match(/\d/g).join("");
+    console.log("year:", year);
+    const data = await fs.readFile(__dirname + "/../names/" + fileName, {
+      encoding: "utf8",
+    });
+    const splitted = data.split("\r\n");
+    for (const o of splitted) {
+      const commaSplit = o.split(",");
+      if (commaSplit[0] && commaSplit[2] > 6500) {
+        var nameData = {
+          name: commaSplit[0],
+          gender: commaSplit[1],
+          year: year,
+          count: commaSplit[2],
+        };
+        allNames.push(nameData);
+      }
+    }
+  }
+  return allNames;
+};
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const allNameData = await parseDataFiles();
-    console.log("allNameData.length", allNameData.length);
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    const allNames = await parseDataFiles();
+    console.log("allNames.length", allNames.length);
+    await Names.bulkCreate(allNames, { returning: false });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete("Names", null, {});
   },
 };
